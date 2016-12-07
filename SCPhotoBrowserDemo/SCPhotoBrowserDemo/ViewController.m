d32fa03ebb915fdb45cd96a322193a41fd642d75
shenchong <569471293@qq.com>
2016-12-07 16:12:29 +0800

    创建工程

---
+++ SCPhotoBrowserDemo/SCPhotoBrowserDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SCPhotoBrowserDemo
+//
+//  Created by easemob on 2016/12/7.
+//  Copyright © 2016年 沈冲. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SCPhotoBrowserDemo/SCPhotoBrowserDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SCPhotoBrowserDemo
+//
+//  Created by easemob on 2016/12/7.
+//  Copyright © 2016年 沈冲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
